aaeaa79d14d67ae1f2b7fec1e4b894056862c008
lisanxing <lisanxing@lexue.com>
2019-07-11 11:05:04 +0800

    加入iviewui（全局引入） 加入配置流程

---
+++ progress.md
@@ -0,0 +1,8 @@
+```
+1: 安装全局的 vue
+npm install -g @vue/cli
+
+
+2：添加iview
+vue add iview
+```
---
+++ src/plugins/iview.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import iView from 'iview'
+
+Vue.use(iView)
+
+import '../theme/less/iview-variables.less'
