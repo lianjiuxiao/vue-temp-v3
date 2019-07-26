6b724393ecf8c14fc5a22497115222479eed100f
lisanxing <lisanxing@lexue.com>
2019-07-26 14:28:00 +0800

    添加错误页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,40 +2,83 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router);
-export default new Router({
+
+const routes = [
+    {
+        path: '/',
+        name: '_home',
+        redirect: '/home',
+        component: () => import('@/views/Index'),
+        meta: {
+            hideInMenu: true,
+            notCache: true
+        },
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                meta: {
+                    hideInMenu: true,
+                    title: '首页',
+                    notCache: true,
+                    icon: 'md-home'
+                },
+                component: () => import('@/views/Home')
+            }
+        ]
+    },
+    {
+        path: '/login',
+        name: 'login',
+        meta: {
+            title: 'Login - 登录',
+            hideInMenu: true
+        },
+        component: () => import('@/views/login/login')
+    },
+    {
+        path: '/401',
+        name: 'error_401',
+        meta: {
+            hideInMenu: true
+        },
+        component: () => import('@/views/error-page/401.vue')
+    },
+    {
+        path: '/500',
+        name: 'error_500',
+        meta: {
+            hideInMenu: true
+        },
+        component: () => import('@/views/error-page/500.vue')
+    },
+    {
+        path: '*',
+        name: 'error_404',
+        meta: {
+            hideInMenu: true
+        },
+        component: () => import('@/views/error-page/404.vue')
+    }
+]
+
+const router = new Router({
     mode: 'hash',
-    routes: [
-        {
-            path: '/',
-            name: '_home',
-            redirect: '/home',
-            component: () => import('@/views/Index'),
-            meta: {
-                hideInMenu: true,
-                notCache: true
-            },
-            children: [
-                {
-                    path: '/home',
-                    name: 'home',
-                    meta: {
-                        hideInMenu: true,
-                        title: '首页',
-                        notCache: true,
-                        icon: 'md-home'
-                    },
-                    component: () => import('@/views/Home')
-                }
-            ]
-        },
-        {
-            path: '/login',
-            name: 'login',
-            meta: {
-                title: 'Login - 登录',
-                hideInMenu: true
-            },
-            component: () => import('@/views/login/login')
-        },
-    ]
+    routes
+});
+
+
+router.beforeEach((to, from, next) => {
+    console.log("beforeEach>>>>>>>>>>>>>>>")
+    console.log(to)
+    console.log(from)
+
+    next()
 })
+
+router.afterEach(to => {
+    console.log("afterEach>>>>>>>>>>>>>>>")
+    console.log(to);
+})
+
+export default router
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,6 +2,7 @@
   <div>
     <router-link :to="{ path: '/my'}">MY</router-link>
     <router-link :to="{ path: '/ui'}">ui</router-link>
+    <router-view/>
   </div>
 </template>
 
